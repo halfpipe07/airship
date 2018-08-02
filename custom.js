89cbfaf5e4bca391348c42bc450b5e160d8c74e3
Rachelle Uy <rachelletan.uy@gmail.com>
2018-08-03 06:49:33 +0800

    Initial commit

---
+++ custom.js
@@ -0,0 +1,99 @@
+(function(window) {
+
+	var util = {
+		numberWithCommas: function(x) {
+			return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		}
+	}
+
+	var main = {
+		stickyNav: function() {
+			window.onscroll = function() {toggleSticky()};
+			var navbar = document.getElementById("airshipNav");
+			var sticky = navbar.offsetTop;
+
+			function toggleSticky() {
+			  if (window.pageYOffset > sticky) {
+			    navbar.classList.add("sticky")
+			  } else {
+			    navbar.classList.remove("sticky");
+			  }
+			}
+		},
+		typewriter: function() {
+			var app = document.getElementById('typed');
+
+			var typewriter = new Typewriter(app, {
+			    loop: true
+			});
+
+			typewriter.typeString('Packages')
+			    .pauseFor(2500)
+			    .deleteAll()
+			    .typeString('Bookings')
+			    .pauseFor(2500)
+			    .deleteAll()
+			    .typeString('Riders')
+			    .pauseFor(2500)
+			    .deleteAll()
+			    .typeString('Clients')
+			    .pauseFor(2500)
+			    .deleteAll()
+			    .start();
+		},
+		priceslider: function() {
+			// price slider
+			var slider = document.getElementById("priceRange");
+			var output = document.getElementById("pkgCount");
+			var price = document.getElementById("priceText");
+
+			var pricing = {
+				1: "0.10",
+				2: "0.09",
+				3: "0.08",
+				4: "0.07",
+				5: "0.06",
+				6: "0.05",
+				7: "0.04",
+				8: "0.04"
+			}
+
+			// Update the current slider value (each time you drag the slider handle)
+			slider.oninput = function() {
+				var val = this.value;
+			    output.innerHTML = util.numberWithCommas((val < 21 ? val : ((val-20)*5)+20 )*1000);
+			    price.innerHTML = pricing[Math.ceil(val/5)];
+			}
+
+			slider.oninput();
+		},
+		linkTo: function(id, classes) {
+			const container = document.getElementById(id);
+			const links = container.getElementsByClassName('features-list')[0].getElementsByTagName('h4');
+
+			for(var i = 0; i < links.length; i++) {
+				links[i].onclick = function(e) {
+					classes.forEach((c) => { container.classList.remove(c); });
+					container.classList.add(e.target.className);
+				}
+			}
+		}
+	}
+
+	window.initHome = function() {
+		main.stickyNav();
+		main.typewriter();
+	}
+
+	window.initPricing = function() {
+		main.stickyNav();
+		main.priceslider();
+	}
+
+	window.initFeatures = function() {
+		main.stickyNav();
+		main.linkTo('toggleBranded', ['branded', 'client', 'branded-app']);
+	}
+
+	
+})(window);
